b63fdb07d06a156cd438303de28f5e204a62f21b
Alexander AULONG <alexander@aulong.fr>
2025-04-21 21:51:01 +0200

    Refactor profile page: cleanup, better security, and UX improvements

--- user/profile.php
+++ user/profile.php
@@ -3,14 +3,42 @@ require_once '../includes/header.php';
 require_once '../config/config.php';
 
 checkConnect();
+
+$id = (int) $_SESSION['id'];
+
+$stmt = $pdo->prepare("SELECT id, lastname, firstname, email, note FROM users WHERE id = ?");
+$stmt->execute([$id]);
+$user = $stmt->fetch();
 ?>
 
     <div id="main-part">
         <h2>My profile</h2>
         <?= displayLockedStatus() ?>
+        <table class="user-table-vertical">
+            <tr>
+                <th>ID</th>
+                <td><?= $user['id'] ?></td>
+            </tr>
+            <tr>
+                <th>LASTNAME</th>
+                <td><?= htmlspecialchars(ucfirst(strtolower($user['lastname']))) ?></td>
+            </tr>
+            <tr>
+                <th>FIRSTNAME</th>
+                <td><?= htmlspecialchars(ucfirst(strtolower($user['firstname']))) ?></td>
+            </tr>
+            <tr>
+                <th>MAIL</th>
+                <td><?= htmlspecialchars($user['email']) ?></td>
+            </tr>
+            <tr>
+                <th>DEBT</th>
+                <td><?= colorDebt((float)$user['note']) ?> €</td>
+            </tr>
+        </table>
         <div class="dashboard_container">
-            <a class="dashboard_item" title="My informations" href="#">📋My informations</a>
             <a class="dashboard_item" title="Change password" href="change_password.php">🔑Change my password</a>
+            <a class="dashboard_item" title="Change mail" href="change_mail.php">📧Change mail</a>
             <a class="dashboard_item" title="Delete my account" href="#">❌Delete my account</a>
         </div>
         <?= backupLink('dashboard.php', '🔙back to dashboard') ?>
